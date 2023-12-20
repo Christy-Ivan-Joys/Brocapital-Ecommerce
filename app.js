a66fe617f24b3f1d664957f24a4c75f905d1281a
Christy Ivan Joys <christyivanjoys@gmail.com>
2023-12-20 13:07:27 +0530

    error page

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ db.connect()
 app.use('/admin', adminRouter)
 app.use('/', usersRouter);
 app.use(errorHandler);
+app.get("*", (req, res) => {
+  res.status(404).render("404")
+})
 
 app.use(function(req, res, next) {
   next(createError(404));
@@ -43,11 +46,10 @@ app.use(function(err, req, res, next) {
  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
-  
